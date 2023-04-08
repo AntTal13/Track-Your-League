480d4d721dceb0bf2b15c15eb86945b5732844a3
AntTal13 <anthonytalarico13@gmail.com>
2023-04-08 12:59:10 -0400

    delete w/o user id implemented yet

--- controllers/games.js
+++ controllers/games.js
@@ -1,5 +1,6 @@
 const Team = require('../models/team');
 const Game = require('../models/game');
+const User = require('../models/user');
 
 module.exports = {
   index,
@@ -9,17 +10,23 @@ module.exports = {
 };
 
 async function index(req, res) {
-  const games = await Game.find({});
+  const games = await Game.find({}).populate('user');
   res.render('games/index', { title: 'My Games', games });
 }
 
 async function deleteGame(req, res) {
-    const game = await Game.findOne({ 'games._id': req.params.id, 'games.user': req.user._id });
-    if (!game) return res.redirect('/games');
-    game.remove(req.params.id);
-    await game.save();
+    // const game = await Game.findOne({ 'games._id': req.params.id, 'games.user': req.user._id });
+    // if (!game) return res.redirect('/games');
+    // game.remove(req.params.id);
+    const game = await Game.findById(req.params.id)
+    await Game.deleteOne(game)
+    // await game.save();
     res.redirect(`/games`);
 }
+
+// Controller Delete by ID around form 
+// <% if (user?._id.equals(g.user)) { %>
+// <% } %>
 
 async function addToGames(req, res) {
     const game = await Game.findById(req.params.id);
@@ -31,12 +38,17 @@ async function addToGames(req, res) {
 async function create(req, res) {
   req.body.born += 'T00:00';
   try {
-    await Game.create(req.body);
+    const game = await Game.create(req.body);
 
     req.body.user = req.user._id;
     req.body.userName = req.user.name;
     req.body.userAvatar = req.user.avatar;
 
+    game.user.push(req.body.user);
+    await game.save()
+    console.log(game)
+    console.log(game.user)
+    console.log(game._id)
   } catch (err) {
     console.log(err);
   }
--- models/game.js
+++ models/game.js
@@ -12,6 +12,10 @@ const gameSchema = new Schema({
         type: Schema.Types.ObjectId,
         ref: 'Stat'
      }],
+     user: [{
+        type: Schema.Types.ObjectId,
+        ref: 'User'
+     }],
 }, {
     timestamps: true
 });
