cc599962adb53ceaf9c2efde43735088a2249495
AntTal13 <anthonytalarico13@gmail.com>
2023-04-11 13:52:38 -0400

    updates

--- controllers/stats.js
+++ controllers/stats.js
@@ -13,20 +13,25 @@ async function create(req, res) {
     const game = await Game.findById(req.params.id).populate('boxScore');;
     const team = await Team.findOne({ 'games': game }).populate('players');
     const players = await Player.find({ _id: { $nin: team.players } }).populate('stats').sort('name');
+    
     // New below
     const newStat = {
-        stat: {
-            points: req.body.points,
-            assists: req.body.assists,
-            rebounds: req.body.rebounds,
-        }
+        players: players._id,
+        game: game._id,
+        points: req.body.points,
+        assists: req.body.assists,
+        rebounds: req.body.rebounds,
     }
 
-    players.stats.push(newStat.stat)
-    game.boxScore.push(newStat.stat)
+    players.stats.push(newStat._id)
+    game.boxScore.push(newStat._id)
+    await players.save()
+    await game.save()
+
     console.log(newStat, 'Hello')
+    
     // New above
-    res.render('stats/show', { title: 'Game Stats', game, team, players });
+    res.render('stats/show', { title: 'Game Stats', game, team, players }); 
 }
 
 async function show(req, res) {
